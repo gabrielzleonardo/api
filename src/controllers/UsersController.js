023dcc127370db4d88387f04f745100dba692f3d
gabriel <gleonardoalano@gmail.com>
2023-02-16 15:33:03 -0300

    fix: removed bug to test API errors

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,13 +1,12 @@
-const AppError = require("../utils/AppError")
+const AppError = require("../utils/AppError");
 
 class UsersController {
   create(request, response) {
-    const { name, email, password } = request.bodyaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaademooooooooooooooooooooonnnnnnnnnniiiiiiiiiiooooooooooooooooooooooooooooooooooooooooooooo;
+    const { name, email, password } = request.body;
 
     if (!name) {
       throw new AppError("Name is required", 400);
     }
-
 
     response.status(201).json({ name, email, password });
   }
