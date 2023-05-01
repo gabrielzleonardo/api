9eb036e337676a5036e746a150979092e9ffb981
gabriel <gleonardoalano@gmail.com>
2023-05-01 09:44:53 -0300

    wip UserAvatarontroller

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,21 @@
 const { Router } = require("express");
+const multer = require("multer");
 const ensureAuthentication = require("../middleware/ensureAuthentication");
 const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
 const userRouter = Router();
+const uploadConfig = require("../configs/upload");
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 userRouter.post("/", usersController.create);
 userRouter.put("/", ensureAuthentication, usersController.update);
+userRouter.patch(
+  "/avatar",
+  ensureAuthentication,
+  upload.single("avatar"),
+  userAvatarController.update
+);
 
 module.exports = userRouter;
